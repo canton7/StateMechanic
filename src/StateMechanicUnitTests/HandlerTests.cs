4b823e40688d707042a835e1849cb4fc02e00224
Antony Male <antony.male@gmail.com>
2014-11-25 08:21:56 +0000

    Couple more tests

--- src/StateMechanicUnitTests/HandlerTests.cs
+++ src/StateMechanicUnitTests/HandlerTests.cs
@@ -15,8 +15,11 @@ namespace StateMechanicUnitTests
         private State state1;
         private State state2;
         private State state3;
+
         private Event event1;
         private Event event2;
+        private Event event3;
+        private Event event4;
 
         private ITransition<State> transition12;
 
@@ -34,11 +37,16 @@ namespace StateMechanicUnitTests
 
             this.event1 = this.sm.CreateEvent("Event 1");
             this.event2 = this.sm.CreateEvent("Event 2");
+            this.event3 = this.sm.CreateEvent("Event 3");
+            this.event4 = this.sm.CreateEvent("Event 4");
 
             this.transition12 = this.state1.AddTransitionOn(this.event1).To(this.state2).WithHandler(x => this.events.Add("Transition 1 2"));
             this.state1.AddTransitionOn(this.event2).To(this.state3).WithHandler(x => this.events.Add("Transition 1 3"));
 
             this.state2.AddTransitionOn(this.event2).To(this.state1).WithHandler(x => this.events.Add("Transition 2 1"));
+
+            this.state1.AddTransitionOn(this.event3).To(this.state1).WithHandler(i => this.events.Add("Transition 1 1"));
+            this.state1.AddInnerSelfTransitionOn(this.event4).WithHandler(i => this.events.Add("Transition 1 1 Inner"));
         }
 
         [Test]
@@ -77,6 +85,22 @@ namespace StateMechanicUnitTests
             this.event1.Fire();
 
             Assert.That(this.events, Is.EquivalentTo(new[] { "State 1 Exit", "Transition 1 2", "State 2 Entry", "State 2 Exit", "Transition 2 1", "State 1 Entry" }));
+        }
+
+        [Test]
+        public void NormalSelfTransitionShouldFireExitAndEntry()
+        {
+            this.event3.Fire();
+
+            Assert.That(this.events, Is.EquivalentTo(new[] { "State 1 Exit", "Transition 1 1", "State 1 Entry" }));
+        }
+
+        [Test]
+        public void InnerSelfTransitionShouldNotFireExitAndEntry()
+        {
+            this.event4.Fire();
+
+            Assert.That(this.events, Is.EquivalentTo(new[] { "Transition 1 1 Inner" }));
         }
     }
 }
