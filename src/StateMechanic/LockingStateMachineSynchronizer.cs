c4b557cf10b2002b4cc57f3966957b4e720dc3bd
Antony Male <antony.male@gmail.com>
2015-08-02 21:29:29 +0100

    Finish off the XML docs

--- src/StateMechanic/LockingStateMachineSynchronizer.cs
+++ src/StateMechanic/LockingStateMachineSynchronizer.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace StateMechanic
 {
+    /// <summary>
+    /// State machine synchronizes which achieves thread-safety by synchronous locking
+    /// </summary>
     public class LockingStateMachineSynchronizer : IStateMachineSynchronizer
     {
         private readonly object lockObject = new object();
 
+        /// <summary>
+        /// Method called whenever an event's Fire or TryFire method is invoked
+        /// </summary>
+        /// <param name="invoker">Delegate which, when invoked, will attempt to fire the event.</param>
+        /// <param name="fireMethod">Value indicating whether the event was fired using Fire or TryFire</param>
+        /// <returns>The value to return from the event's TryFire method, if that was used to fire the event</returns>
         public bool FireEvent(Func<bool> invoker, EventFireMethod fireMethod)
         {
             lock (this.lockObject)
@@ -18,6 +27,10 @@ namespace StateMechanic
             }
         }
 
+        /// <summary>
+        /// Method called when ForceTransition is invoked
+        /// </summary>
+        /// <param name="invoker">Method which will cause the forced transition to occur</param>
         public void ForceTransition(Action invoker)
         {
             lock (this.lockObject)
@@ -26,6 +39,10 @@ namespace StateMechanic
             }
         }
 
+        /// <summary>
+        /// Method called when Reset is invoked
+        /// </summary>
+        /// <param name="invoker">Method which will cause the reset to occur</param>
         public void Reset(Action invoker)
         {
             lock (this.lockObject)
--- src/StateMechanic/Transition.cs
+++ src/StateMechanic/Transition.cs
@@ -103,20 +103,47 @@ namespace StateMechanic
         }
     }
 
+    /// <summary>
+    /// A transition from one state to another, triggered by an event
+    /// </summary>
+    /// <typeparam name="TState">Type of state which this transition is between</typeparam>
     public class Transition<TState> : ITransition<TState>, IInvokableTransition where TState : class, IState
     {
         private readonly ITransitionInner<TState, Event, TransitionInfo<TState>> innerTransition;
 
+        /// <summary>
+        /// Gets the state this transition is from
+        /// </summary>
         public TState From { get { return this.innerTransition.From; } }
+
+        /// <summary>
+        /// Gets the state this transition to
+        /// </summary>
         public TState To { get { return this.innerTransition.To; } }
+
+        /// <summary>
+        /// Gets the event which triggers this transition
+        /// </summary>
         public Event Event { get { return this.innerTransition.Event; } }
         IEvent ITransition<TState>.Event { get { return this.innerTransition.Event; } }
+
+        /// <summary>
+        /// Gets a value indicating whether this transition is an inner transition, i.e. whether the <see cref="From"/> and <see cref="To"/> states are the same, and no exit/entry handles are invoked
+        /// </summary>
         public bool IsInnerTransition { get { return this.innerTransition.IsInnerTransition; } }
+
+        /// <summary>
+        /// Gets or sets a method which is invoked whenever this transition occurs
+        /// </summary>
         public Action<TransitionInfo<TState>> Handler
         {
             get { return this.innerTransition.Handler; }
             set { this.innerTransition.Handler = value; }
         }
+
+        /// <summary>
+        /// Gets or sets a method which is invoked before this transition occurs, and can prevent the transition from occuring
+        /// </summary>
         public Func<TransitionInfo<TState>, bool> Guard
         {
             get { return this.innerTransition.Guard; }
@@ -128,12 +155,22 @@ namespace StateMechanic
             this.innerTransition = innerTransition;
         }
 
+        /// <summary>
+        /// Sets a method which is invoked whenever this transition occurs
+        /// </summary>
+        /// <param name="handler">Method which is invoked whenever this transition occurs</param>
+        /// <returns>This transition, for method chaining</returns>
         public Transition<TState> WithHandler(Action<TransitionInfo<TState>> handler)
         {
             this.Handler = handler;
             return this;
         }
 
+        /// <summary>
+        /// Sets a method which is invoked before this transition occurs, and can prevent the transition from occuring
+        /// </summary>
+        /// <param name="guard">method which is invoked before this transition occurs, and can prevent the transition from occuring</param>
+        /// <returns>This transition, for method chaining</returns>
         public Transition<TState> WithGuard(Func<TransitionInfo<TState>, bool> guard)
         {
             this.Guard = guard;
@@ -147,20 +184,48 @@ namespace StateMechanic
         }
     }
 
+    /// <summary>
+    /// A transition from one state to another, triggered by an event
+    /// </summary>
+    /// <typeparam name="TState">Type of state which this transition is between</typeparam>
+    /// <typeparam name="TEventData">Type of event data associated with the event which triggers this transition</typeparam>
     public class Transition<TState, TEventData> : ITransition<TState>, IInvokableTransition<TEventData> where TState : class, IState
     {
         private readonly ITransitionInner<TState, Event<TEventData>, TransitionInfo<TState, TEventData>> innerTransition;
 
+        /// <summary>
+        /// Gets the state this transition is from
+        /// </summary>
         public TState From { get { return this.innerTransition.From; } }
+
+        /// <summary>
+        /// Gets the state this transition to
+        /// </summary>
         public TState To { get { return this.innerTransition.To; } }
+
+        /// <summary>
+        /// Gets the event which triggers this transition
+        /// </summary>
         public Event<TEventData> Event { get { return this.innerTransition.Event; } }
         IEvent ITransition<TState>.Event { get { return this.innerTransition.Event; } }
+
+        /// <summary>
+        /// Gets a value indicating whether this transition is an inner transition, i.e. whether the <see cref="From"/> and <see cref="To"/> states are the same, and no exit/entry handles are invoked
+        /// </summary>
         public bool IsInnerTransition { get { return this.innerTransition.IsInnerTransition; } }
+
+        /// <summary>
+        /// Gets or sets a method which is invoked whenever this transition occurs
+        /// </summary>
         public Action<TransitionInfo<TState, TEventData>> Handler
         {
             get { return this.innerTransition.Handler; }
             set { this.innerTransition.Handler = value; }
         }
+
+        /// <summary>
+        /// Gets or sets a method which is invoked before this transition occurs, and can prevent the transition from occuring
+        /// </summary>
         public Func<TransitionInfo<TState, TEventData>, bool> Guard
         {
             get { return this.innerTransition.Guard; }
@@ -172,12 +237,22 @@ namespace StateMechanic
             this.innerTransition = innerTransition;
         }
 
+        /// <summary>
+        /// Sets a method which is invoked whenever this transition occurs
+        /// </summary>
+        /// <param name="handler">Method which is invoked whenever this transition occurs</param>
+        /// <returns>This transition, for method chaining</returns>
         public Transition<TState, TEventData> WithHandler(Action<TransitionInfo<TState, TEventData>> handler)
         {
             this.Handler = handler;
             return this;
         }
 
+        /// <summary>
+        /// Sets a method which is invoked before this transition occurs, and can prevent the transition from occuring
+        /// </summary>
+        /// <param name="guard">method which is invoked before this transition occurs, and can prevent the transition from occuring</param>
+        /// <returns>This transition, for method chaining</returns>
         public Transition<TState, TEventData> WithGuard(Func<TransitionInfo<TState, TEventData>, bool> guard)
         {
             this.Guard = guard;
