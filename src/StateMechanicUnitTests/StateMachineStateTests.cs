334168200713847a508bdcd9b88acba3d5077cc8
Antony Male <antony.male@gmail.com>
2015-08-02 21:50:45 +0100

    Couple more unit tests on state machine states

--- src/StateMechanicUnitTests/StateMachineStateTests.cs
+++ src/StateMechanicUnitTests/StateMachineStateTests.cs
@@ -12,6 +12,42 @@ namespace StateMechanicUnitTests
     public class StateMachineStateTests
     {
         [Test]
+        public void StateMachineStartsInInitialState()
+        {
+            var sm = new StateMachine("state machine");
+            var initialState = sm.CreateInitialState("initial state");
+            var state1 = sm.CreateState("state1");
+
+            Assert.AreEqual(initialState, sm.CurrentState);
+            Assert.AreEqual(initialState, sm.InitialState);
+        }
+
+        [Test]
+        public void ChildStateMachineStartsInInitialStateIfParentStateIsInitialState()
+        {
+            var parent = new StateMachine("parent");
+            var initial = parent.CreateInitialState("initial");
+            var child = initial.CreateChildStateMachine("child");
+            var childInitial = child.CreateInitialState("childInitial");
+
+            Assert.AreEqual(childInitial, child.CurrentState);
+            Assert.AreEqual(childInitial, child.InitialState);
+        }
+
+        [Test]
+        public void ChildStateMachineStartsInNoStateIfParentStateIsNotInitialState()
+        {
+            var parent = new StateMachine("parent");
+            var initial = parent.CreateInitialState("initial");
+            var state1 = parent.CreateState("state1");
+            var child = state1.CreateChildStateMachine("child");
+            var childInitial = child.CreateInitialState("childInitial");
+
+            Assert.AreEqual(null, child.CurrentState);
+            Assert.AreEqual(childInitial, child.InitialState);
+        }
+
+        [Test]
         public void ResetResetsStateOfChildStateMachines()
         {
             var parent = new StateMachine("parent");
